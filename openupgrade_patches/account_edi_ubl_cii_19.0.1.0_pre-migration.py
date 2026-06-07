0304ec5f27827addb2e55f862489a636aaac718f
Markus Badberg <markus@badhills.de>
2026-06-07 21:48:59 +0200

    Fix account_edi_ubl_cii 19.0 pre-migration to map from legacy column

---
+++ openupgrade_patches/account_edi_ubl_cii_19.0.1.0_pre-migration.py
@@ -0,0 +1,32 @@
+# Copyright 2026 Tecnativa - Eduardo Ezerouali
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+_reason_code_map = [
+    ("VATEX_EU_AE", "VATEX-EU-AE"),
+    ("VATEX_EU_D", "VATEX-EU-D"),
+    ("VATEX_EU_F", "VATEX-EU-F"),
+    ("VATEX_EU_G", "VATEX-EU-G"),
+    ("VATEX_EU_I", "VATEX-EU-I"),
+    ("VATEX_EU_IC", "VATEX-EU-IC"),
+    ("VATEX_EU_J", "VATEX-EU-J"),
+    ("VATEX_EU_O", "VATEX-EU-O"),
+    ("VATEX_FR-CNWVAT", "VATEX-FR-CNWVAT"),
+    ("VATEX_FR-FRANCHISE", "VATEX-FR-FRANCHISE"),
+]
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.copy_columns(
+        env.cr,
+        {"account_tax": [("ubl_cii_tax_exemption_reason_code", None, None)]},
+    )
+    openupgrade.map_values(
+        env.cr,
+        openupgrade.get_legacy_name("ubl_cii_tax_exemption_reason_code"),
+        "ubl_cii_tax_exemption_reason_code",
+        _reason_code_map,
+        table="account_tax",
+    )
